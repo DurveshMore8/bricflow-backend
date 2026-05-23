a76665d865d316cc271c18b50fc790a63129e700
Durvesh More <durveshmore2003@gmail.com>
2026-05-23 21:11:37 +0530

    feat: initialize backend architecture

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import compression from "compression";
+import cors from "cors";
+import express from "express";
+import helmet from "helmet";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use(helmet());
+
+app.use(compression());
+
+export default app;
